15770cc29340caa0bf6b4043249b4900602f6c3e
Neha Shrivastava <Neha Shrivastava@DESKTOP-G1QRJA5>
2022-05-12 14:10:40 -0400

    Hello blog

---
+++ BlogApp/src/main/java/com/learning/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.learning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String home(Model m)
+	{
+		String str = "HOME";
+		m.addAttribute("name", str);
+		return "home";  //view
+
+	}
+
+}
